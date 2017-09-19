6ac5b005b55916c8efabf9fe38e6d15c10632336
Kevin Duck <kevinduck@lindsayuartsair.lan>
2017-09-19 16:31:49 -0700

    obscure api key

--- ml.py
+++ ml.py
@@ -1,19 +1,23 @@
 # Machine learning modules and implementations
+# Linear Regression used to predict stock prices
+
 import datetime
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
 from matplotlib import style
 import numpy as np
+import pickle
 import quandl
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
 import time
+import quandl_api_key
 
-style.use('ggplot')
+style.use('fivethirtyeight')
 
-quandl.ApiConfig.api_key = "xXE3jyYzZivcBnNHTnBM"
-ticker = raw_input('Enter ticker symbol: ')
+quandl.ApiConfig.api_key = quandl_api_key.get_key()
+ticker = raw_input('Enter ticker symbol: ').upper()
 df = quandl.get('WIKI/{}'.format(ticker))
 
 df = df[['Adj. Open', 'Adj. High', 'Adj. Close', 'Adj. Volume']]
@@ -26,7 +30,7 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 # Extend forecast out by 1% more of the total data
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
@@ -41,19 +45,22 @@ df.dropna(inplace=True)
 
 # Create label column
 y = np.array(df['label'])
-y = np.array(df['label'])
-
 
 # Shuffle up data and use 20% as training data
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 # n_jobs is optional; sets number of processes you run at once; -1 uses as many as possible
 # clf = LinearRegression(n_jobs=-1)
-clf = LinearRegression()
+#
+# # Easy to switch to another classifier:
+# # clf = svm.SVR()
+# clf.fit(X_train, y_train)
+#
+# with open ('linearregression.pickle', 'wb') as f:
+#     pickle.dump(clf, f)
 
-# Easy to switch to another classifier:
-# clf = svm.SVR()
-clf.fit(X_train, y_train)
-clf.score(X_test, y_test)
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(X_test, y_test)
 # print("Forecast_out: {} days".format(forecast_out))
 
@@ -69,7 +76,6 @@ df['forecast'] = np.nan
 last_date = df.iloc[-1].name
 last_unix = last_date.to_datetime()
 last_unix = time.mktime(last_unix.timetuple())
-print type(last_unix)
 one_day = 86400
 next_unix = last_unix + one_day
 
@@ -83,6 +89,7 @@ for i in forecast_set:
 df['Adj. Close'].plot()
 df['forecast'].plot()
 plt.legend(loc=4)
+plt.title('{} Stock Price + {} days ({}%)'.format(ticker, forecast_out, (np.rint(accuracy*100))))
 plt.xlabel('Date')
 plt.ylabel('Price')
 plt.show()
